ba28c5050239612a75343cf5f6cc4c468d2a7fb5
Daniel Czeschner <daniel.czeschner@web.de>
2022-11-15 15:46:12 +0100

    Hello display snippet to test the pcb

--- Controller/Snippets/20_Display/20_hello_display/20_hello_display.ino
+++ Controller/Snippets/20_Display/20_hello_display/20_hello_display.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
 
+static const int OLED_RESET = 4;
+Adafruit_SH1106 display(OLED_RESET);
+
+void setup()   {
+  Serial.begin(9600);
+  display.begin(SH1106_SWITCHCAPVCC, 0x3D);
+  display.clearDisplay();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  long runtime = millis() / 1000;
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
 
+  display.setCursor(25, 0);
+  display.println("Hello display!");
+  
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.println(runtime);
+
+  display.setTextSize(1);
+  display.setTextColor(BLACK, WHITE);
+  display.setCursor(20, 25);
+  display.println("FireKey");
+  
+  display.display();
+    
+  display.clearDisplay();
 }
