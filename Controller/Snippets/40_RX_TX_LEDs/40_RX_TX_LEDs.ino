e35cc2d3b034527e541fc86f3e749909974f3677
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-11-16 20:30:19 +0100

    added comments RX_TX_LEDs

--- Controller/Snippets/40_RX_TX_LEDs/40_RX_TX_LEDs.ino
+++ Controller/Snippets/40_RX_TX_LEDs/40_RX_TX_LEDs.ino
@@ -3,6 +3,10 @@
 * 
 * Code snippet to test turining off the RX/TX LEDs.
 * 
+* See:
+* - https://arduino.stackexchange.com/questions/63446/arduino-pro-micro-switch-off-leds
+* - https://github.com/arduino/ArduinoCore-avr/blob/master/variants/micro/pins_arduino.h (does not work)
+*
 * Requried libs:
 * - None
 *********************************************************************/
