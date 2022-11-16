43d05da22da3e16b68bd3d32a2c4f54c97f6caa6
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-11-16 17:00:41 +0100

    worked on led_test snippet

--- Controller/Snippets/10_LEDs/10_led_test_V1/10_led_test_V1.ino
+++ Controller/Snippets/10_LEDs/10_led_test_V1/10_led_test_V1.ino
@@ -1,39 +1,70 @@
 /**
-* Test snippet to test the led strip hardware.
-* The program iterates over each LED and switches one led more (green) on
+* FIREKEY - PROJECT
+* 
+* Code snippet to test the LED strip hardware component.
+* The program iterates over each LED and switches one LED more (green) on.
 * 
 * Requried libs:
-* Adafruit NeoPixel Lib(https://github.com/adafruit/Adafruit_NeoPixel). Can be installed via Arduino IDE
+* - Adafruit NeoPixel (https://github.com/adafruit/Adafruit_NeoPixel) can be installed via Arduino IDE
 */
 
 #include <Adafruit_NeoPixel.h>
 
-// The pin of the controller to which the led strip is connected
-static const int LED_PIN = 21;
-// The amount of leds in the led strip
-static const int NUM_LEDS = 6;
+// pin connected to DIN of the LED strip
+const int LED_PIN = 21;  // A3
 
-// Setup led strip lib
-Adafruit_NeoPixel led_strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+// amount of LEDs in the LED strip
+const int NUM_LEDS = 6;
+
+// LED brightness
+const int LED_BRIGHTNEESS = 20;
+
+// get LED strip object
+Adafruit_NeoPixel ledStrip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
-  // INITIALIZE led strip object
-  led_strip.begin();
+  // initialize LED strip object
+  ledStrip.begin();
 }
 
+
 void loop() {
-  // Turn all leds off
-  led_strip.clear();
+  // turn all LEDs off
+  ledStrip.clear();
 
-  // For each led...
-  for(int i=0; i<NUM_LEDS; i++) {
-    // Set led-color to green
-    led_strip.setPixelColor(i, 0, 60, 0);
+  // for each LED ...
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // set LED color to green
+    ledStrip.setPixelColor(i, 0, LED_BRIGHTNEESS, 0);
 
-    // Send the updated pixel colors to the hardware
-    led_strip.show();
+    // send the updated pixel colors to the hardware
+    ledStrip.show();
 
-    // Wait 500 ms
-    delay(500);
+    delay(1000);
   }
+
+  // for each LED ...
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // set LED color to red
+    ledStrip.setPixelColor(i, LED_BRIGHTNEESS, 0, 0);
+
+    // send the updated pixel colors to the hardware
+    ledStrip.show();
+
+    delay(1000);
+  }
+
+  // for each LED ...
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // set LED color to blue
+    ledStrip.setPixelColor(i, 0, 0, LED_BRIGHTNEESS);
+
+    // send the updated pixel colors to the hardware
+    ledStrip.show();
+
+    delay(1000);
+  }
+
+  delay(1000);
 }
