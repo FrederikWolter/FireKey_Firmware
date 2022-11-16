2e174a0eac88a1ca0033c437ef68b5b82b3a1332
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-11-16 19:22:03 +0100

    worked on hello_display snippet

--- Controller/Snippets/20_Display/20_hello_display/20_hello_display.ino
+++ Controller/Snippets/20_Display/20_hello_display/20_hello_display.ino
@@ -1,36 +1,47 @@
-/**
-* Snippet to test the oled display hardware.
+/*********************************************************************
+* FIREKEY - PROJECT
+*
+* Code snippet to test the oled display hardware component.
 *
 * Required Libs:
-* Adafruit SH1106 Lib(https://github.com/wonho-maker/Adafruit_SH1106). Is located in the 00_Lib folder.
-* Adafruit GFX Lib(https://github.com/adafruit/Adafruit-GFX-Library). Can be installed via Arduino IDE.
-*/
+* - Adafruit SH1106 Lib(https://github.com/wonho-maker/Adafruit_SH1106)
+*    -> is located in the 00_Lib folder
+* - Adafruit GFX Lib(https://github.com/adafruit/Adafruit-GFX-Library)
+*    -> can be installed via Arduino IDE
+*********************************************************************/
 
-#include <SPI.h>
-#include <Wire.h>
+// LIBRARIES
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
 
-static const int OLED_RESET = 4;
+// PINS
+#define OLED_RESET -1
+
+// CONSTANTS
+
+// OLED object
 Adafruit_SH1106 display(OLED_RESET);
+
 
 void setup()   {
   Serial.begin(9600);
-  display.begin(SH1106_SWITCHCAPVCC, 0x3D);
+  Serial.println("i am alive");
+  delay(1000);
+
+  display.begin(SH1106_SWITCHCAPVCC, 0x3C);
+  Serial.println("diplay started");
   display.clearDisplay();
 }
 
+
 void loop() {
-  long runtime = millis() / 1000;
   display.setTextSize(1);
   display.setTextColor(WHITE);
-
   display.setCursor(25, 0);
   display.println("Hello display!");
   
-  display.setTextColor(WHITE);
   display.setTextSize(2);
-  display.println(runtime);
+  display.println(millis() / 1000);
 
   display.setTextSize(1);
   display.setTextColor(BLACK, WHITE);
@@ -38,6 +49,5 @@ void loop() {
   display.println("FireKey");
   
   display.display();
-    
-  display.clearDisplay();
+  delay(1000);
 }
