6a0c29a6710dbf1894235a0cef5051a513ec4de5
Daniel Czeschner <daniel.czeschner@web.de>
2022-11-16 18:13:24 +0100

    First metric test

--- Controller/Snippets/30_KeyMetric/30_6_keys_metric_test/30_6_keys_metric_test.ino
+++ Controller/Snippets/30_KeyMetric/30_6_keys_metric_test/30_6_keys_metric_test.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+byte rows[] = { 5, 6 };
+byte columns[] = { 10, 16, 14 };
 
+const int rowCount = sizeof(rows) / sizeof(rows[0]);
+const int colCount = sizeof(columns) / sizeof(columns[0]);
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int x = 0; x < colCount; x++) {
+    pinMode(columns[x], OUTPUT);
+  }
+
+  for (int x = 0; x < rowCount; x++) {
+    pinMode(rows[x], INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readMatrix();
+}
 
+void keyPressed(int rowIdx, int colIdx) {
+  Serial.print("Row: ");
+  Serial.print(rowIdx);
+  Serial.print(" Col: ");  
+  Serial.print(colIdx);
+  Serial.println(" pressed!");
+}
+
+void readMatrix() {
+  // iterate the rows
+  for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+    // row: set to output to low
+    byte curRow = rows[rowIndex];
+    digitalWrite(curRow, LOW);
+
+    // col: interate through the columns
+    for (int colIndex = 0; colIndex < colCount; colIndex++) {
+      byte colRow = columns[colIndex];
+      int keyStatus = digitalRead(colRow);
+      if (keyStatus == LOW) {
+        keyPressed(rowIndex, colIndex);
+      }
+    }
+  }
 }
