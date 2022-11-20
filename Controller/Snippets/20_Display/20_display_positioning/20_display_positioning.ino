ccd20b60b384aeada46cfaae7d4288c4168b1c2e
Daniel Czeschner <daniel.czeschner@web.de>
2022-11-20 18:56:09 +0100

    Worked on display positioning snippet

---
+++ Controller/Snippets/20_Display/20_display_positioning/20_display_positioning.ino
@@ -0,0 +1,106 @@
+/*********************************************************************
+* FIREKEY - PROJECT
+*
+* Code snippet which displays the button metric functions
+*
+* Required Libs:
+* - Adafruit SH1106 Lib(https://github.com/wonho-maker/Adafruit_SH1106)
+*    -> is located in the 00_Lib folder
+* - Adafruit GFX Lib(https://github.com/adafruit/Adafruit-GFX-Library)
+*    -> can be installed via Arduino IDE
+*********************************************************************/
+
+// LIBRARIES
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+
+// PINS
+#define OLED_RESET -1  // use no extra reset pin
+
+// CONSTANTS
+#define OLED_ADDR 0x3C
+
+typedef enum xPos {
+  LEFT = 0,
+  VLINE1 = 41,
+  CENTER = 64,
+  VLINE2 = 85,
+  RIGHT = 127
+} xPosition;
+
+typedef enum yPos {
+  TOP = 0,
+  ROW0 = 2,
+  HLINE1 = 11,
+  ROW1 = 16,
+  ROW2 = 28,
+  ROW3 = 40,
+  ROW4 = 52,
+  BOTTOM = 63
+} yPosition;
+
+// OLED object
+Adafruit_SH1106 display(OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting ...");
+
+  display.begin(SH1106_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  Serial.println("display started");
+
+  displayShowMetricFuncs();
+}
+
+
+void loop() {
+}
+
+void drawCentreString(const char *buf, xPosition xPosition, yPosition yPosition) {
+  int16_t x1, y1;
+  uint16_t w, h;
+  display.getTextBounds(buf, xPosition, yPosition, &x1, &y1, &w, &h);  //calc width of new string
+
+  if (xPosition == LEFT) {
+    display.setCursor(LEFT, yPosition);
+  } else if (xPosition == CENTER) {
+    int xVal = CENTER - w / 2;
+    display.setCursor(xVal, yPosition);
+  } else if (xPosition == RIGHT) {
+    int xVal = RIGHT - w;
+    display.setCursor(xVal, yPosition);
+  }
+  display.print(buf);
+}
+
+void displayShowMetricFuncs() {
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  drawCentreString("Prev", LEFT, ROW0);
+  drawCentreString("Home", CENTER, ROW0);
+  drawCentreString("Next", RIGHT, ROW0);
+
+  display.drawLine(LEFT, HLINE1, RIGHT, HLINE1, WHITE);
+  display.drawLine(VLINE1, TOP, VLINE1, BOTTOM, WHITE);
+  display.drawLine(VLINE2, TOP, VLINE2, BOTTOM, WHITE);
+
+  drawCentreString("Action", LEFT, ROW1);
+  drawCentreString("Action", CENTER, ROW1);
+  drawCentreString("Action", RIGHT, ROW1);
+
+  drawCentreString("Action", LEFT, ROW2);
+  drawCentreString("Action", CENTER, ROW2);
+  drawCentreString("Action", RIGHT, ROW2);
+
+  drawCentreString("Action", LEFT, ROW3);
+  drawCentreString("Action", CENTER, ROW3);
+  drawCentreString("Action", RIGHT, ROW3);
+
+  drawCentreString("Action", LEFT, ROW4);
+  drawCentreString("Action", CENTER, ROW4);
+  drawCentreString("Action", RIGHT, ROW4);
+
+  display.display();
+}
