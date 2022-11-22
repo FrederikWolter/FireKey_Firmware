a1d151c870ca84a3f69019a356120cf6d235e468
Daniel Czeschner <daniel.czeschner@web.de>
2022-11-22 19:21:21 +0100

    worked on layer array

--- Controller/Snippets/20_Display/20_display_layer-array/20_display_layer-array.ino
+++ Controller/Snippets/20_Display/20_display_layer-array/20_display_layer-array.ino
@@ -34,6 +34,38 @@ const byte homeLayer = 0;
 
 byte currentLayer = 0;
 
+char layerNames[5][7] = {
+  "Layer1",
+  "Layer2",
+  "Layer3",
+  "Layer4",
+  "Layer5",
+};
+
+// Uses as second index the key index which is the led index
+char layerButtonFunc[5][12][5] = {
+  { "L0L1", "L0M1", "L0R1",  // name: Layer 0 Left Button Row 1; Layer 0 Middle Button Row 1; Layer 0 Middle Button Row 1
+    "L0R2", "L0M2", "L0L2",
+    "L0L3", "L0M3", "L0R3",
+    "L0R4", "L0M4", "L0L4" },
+  { "L1L1", "L1M1", "L1R1",
+    "L1R2", "L1M2", "L1L2",
+    "L1L3", "L1M3", "L1R3",
+    "L1R4", "L1M4", "L1L4" },
+  { "L2L1", "L2M1", "L2R1",
+    "L2R2", "L2M2", "L2L2",
+    "L2L3", "L2M3", "L2R3",
+    "L2R4", "L2M4", "L2L4" },
+  { "L3L1", "L3M1", "L3R1",
+    "L3R2", "L3M2", "L3L2",
+    "L3L3", "L3M3", "L3R3",
+    "L3R4", "L3M4", "L3L4" },
+  { "L4L1", "L4M1", "L4R1",
+    "L4R2", "L4M2", "L4L2",
+    "L4L3", "L4M3", "L4R3",
+    "L4R4", "L4M4", "L4L4" },
+};
+
 typedef enum xPos {
   LEFT = 0,
   VLINE1 = 41,
@@ -59,6 +91,10 @@ Adafruit_SH1106 display(OLED_RESET);
 void setup() {
   Serial.begin(9600);
 
+  display.begin(SH1106_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  Serial.println("display started");
+
   // declare all cols as input and activate internal pullup resistor
   for (int x = 0; x < COL_COUNT; x++) {
     pinMode(cols[x], INPUT_PULLUP);
@@ -69,6 +105,9 @@ void setup() {
     pinMode(rows[x], OUTPUT);
     digitalWrite(rows[x], HIGH);
   }
+
+  Serial.println("metirc initialized");
+  setDisplayText();
 }
 
 void loop() {
@@ -124,6 +163,18 @@ void keyPressed(byte rowIdx, byte colIdx) {
   }
 }
 
+byte getLedIndex(byte rowIdx, byte colIdx) {
+  // calculate led index out of row and col index
+  byte index = rowIdx * COL_COUNT;
+
+  if (rowIdx % 2 == 0) {  // even row?
+    index += colIdx;
+  } else {  // odd row?
+    index += COL_COUNT - 1 - colIdx;
+  }
+  return index;
+}
+
 void drawStringAtPosition(const char *buf, xPosition xPosition, yPosition yPosition) {
   int16_t x1, y1;
   uint16_t w, h;
@@ -142,32 +193,31 @@ void drawStringAtPosition(const char *buf, xPosition xPosition, yPosition yPosit
 }
 
 void setDisplayText() {
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
-  drawStringAtPosition("Prev", LEFT, ROW0);
-  drawStringAtPosition("Home", CENTER, ROW0);
-  drawStringAtPosition("Next", RIGHT, ROW0);
+  drawStringAtPosition(layerNames[currentLayer], CENTER, ROW0);
 
   display.drawLine(LEFT, HLINE1, RIGHT, HLINE1, WHITE);
-  display.drawLine(VLINE1, TOP, VLINE1, BOTTOM, WHITE);
-  display.drawLine(VLINE2, TOP, VLINE2, BOTTOM, WHITE);
+  display.drawLine(VLINE1, HLINE1, VLINE1, BOTTOM, WHITE);
+  display.drawLine(VLINE2, HLINE1, VLINE2, BOTTOM, WHITE);
 
-  drawStringAtPosition("Action", LEFT, ROW1);
-  drawStringAtPosition("Action", CENTER, ROW1);
-  drawStringAtPosition("Action", RIGHT, ROW1);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(1, 2) - 3], LEFT, ROW1);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(1, 1) - 3], CENTER, ROW1);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(1, 0) - 3], RIGHT, ROW1);
 
-  drawStringAtPosition("Action", LEFT, ROW2);
-  drawStringAtPosition("Action", CENTER, ROW2);
-  drawStringAtPosition("Action", RIGHT, ROW2);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(2, 2) - 3], LEFT, ROW2);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(2, 1) - 3], CENTER, ROW2);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(2, 0) - 3], RIGHT, ROW2);
 
-  drawStringAtPosition("Action", LEFT, ROW3);
-  drawStringAtPosition("Action", CENTER, ROW3);
-  drawStringAtPosition("Action", RIGHT, ROW3);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(3, 2) - 3], LEFT, ROW3);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(3, 1) - 3], CENTER, ROW3);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(3, 0) - 3], RIGHT, ROW3);
 
-  drawStringAtPosition("Action", LEFT, ROW4);
-  drawStringAtPosition("Action", CENTER, ROW4);
-  drawStringAtPosition("Action", RIGHT, ROW4);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(4, 2) - 3], LEFT, ROW4);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(4, 1) - 3], CENTER, ROW4);
+  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(4, 0) - 3], RIGHT, ROW4);
 
   display.display();
 }
