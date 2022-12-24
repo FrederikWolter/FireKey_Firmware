db4d4178e74c5fabf31346b8c3194f5d25237c32
Daniel Czeschner <daniel.czeschner@web.de>
2022-12-24 11:35:05 +0100

    Added the first constants and the keys array

--- Controller/Firmware/Firmware.ino
+++ Controller/Firmware/Firmware.ino
@@ -23,15 +23,47 @@
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
-#include "00_Basic.h"
 #include "00_Key.h"
+
+// Begin CONSTANTS
+#define ROW_COUNT 5  // number of rows
+#define COL_COUNT 3  // number of columns
+
+// Layers
+#define LAYER_BACK_KEY 2     // define the back button led index
+#define LAYER_HOME_KEY 1     // define the home button led index
+#define LAYER_FORWARD_KEY 0  // define the forward button led index
+#define MAX_LAYER 5
+#define HOME_LAYER 0
+
+// Led
+#define NUM_LEDS 6  // number of LEDs in the strip
+#define LED_PIN 21
+
+// End CONSTANTS
+
+// LED strip object
+Adafruit_NeoPixel ledStrip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+Key keys[1][1];
+
+byte getLedIndex(byte rowIdx, byte colIdx) {
+  // calculate led index out of row and col index
+  byte index = rowIdx * COL_COUNT;
+  index += rowIdx % 2 == 0 ? colIdx : COL_COUNT - 1 - colIdx;
+  return index;
+}
+
 
 void setup() {
   // put your setup code here, to run once:
-
+  for (byte r = 0; r < ROW_COUNT; r++) {
+    for (byte c = 0; c < COL_COUNT; c++) {
+      keys[r][c] = Key(r, c, getLedIndex(r, c), ledStrip);
+    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
