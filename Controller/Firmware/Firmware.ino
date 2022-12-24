8df07510ff33fe7129293338148d4f988648ab9f
Daniel Czeschner <daniel.czeschner@web.de>
2022-12-24 11:18:10 +0100

    Created Firmware

---
+++ Controller/Firmware/Firmware.ino
@@ -0,0 +1,37 @@
+/*********************************************************************
+* FIREKEY - PROJECT
+* 
+* Firmware Version 0.1V
+* 
+* Required libs:
+* - Keyboard(https://github.com/arduino-libraries/Keyboard)
+*   -> can be installed via arduino ide (defauled installed)
+* - USBHost
+*   -> can be installed via arduino ide
+* - Adafruit NeoPixel (https://github.com/adafruit/Adafruit_NeoPixel) 
+*    -> can be installed via Arduino IDE
+* - Adafruit SH1106 Lib(https://github.com/wonho-maker/Adafruit_SH1106)
+*    -> is located in the 20_Display folder
+* - Adafruit GFX Lib(https://github.com/adafruit/Adafruit-GFX-Library)
+*    -> can be installed via Arduino IDE
+*********************************************************************/
+
+// LIBRARIES
+#include <Keyboard.h>
+#include <Keyboard_de_DE.h>
+#include <KeyboardLayout.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+#include "00_Basic.h"
+#include "00_Key.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
