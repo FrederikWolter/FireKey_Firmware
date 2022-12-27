56e904c05e02db9c194c6abf9e8591b8a0192d98
Daniel Czeschner <daniel.czeschner@web.de>
2022-12-27 13:31:42 +0100

    memory save variant of the display layer text handling

--- Controller/Snippets/20_Display/20_U8g2/20_U8g2_display_layer-array_memory_save/20_U8g2_display_layer-array_memory_save.ino
+++ Controller/Snippets/20_Display/20_U8g2/20_U8g2_display_layer-array_memory_save/20_U8g2_display_layer-array_memory_save.ino
@@ -1,16 +1,20 @@
 /*********************************************************************
 * FIREKEY - PROJECT
 * 
-* Code snippet to test the display for multi layer usage using arrays
+* Code snippet to test the display for multi layer usage using arrays (save memory)
 * 
 * Required Libs:
 * - U8g2 Lib(https://github.com/olikraus/u8g2)
 *    -> can be installed via Arduino IDE
+* - avr/pgmspace lib
+*    -> is already installed (arduino pro micro)
 *********************************************************************/
 
 // LIBRARIES
 #include <U8g2lib.h>
 #include <MemoryFree.h>
+#include <avr/pgmspace.h>
+#include <string.h>
 
 // CONSTANTS
 #define ROW_COUNT 2     // number of rows
@@ -19,6 +23,7 @@
 #define OLED_ADDR 0x3C  //i2c address
 
 #define MAX_KEY_LENGTH 5
+#define LAYER_NAME_LENGTH 10
 //LAYERS
 #define LAYER_BACK_COL_KEY_IDX 2     // define the back button key index
 #define LAYER_HOME_COL_KEY_IDX 1     // define the home button key index
@@ -32,7 +37,7 @@ byte cols[COL_COUNT] = { 10, 16, 14 };  // define the column pins
 
 byte currentLayer = 0;
 
-char layerNames[MAX_LAYER][10] = {
+const char layerNames[MAX_LAYER][LAYER_NAME_LENGTH] PROGMEM = {
   "Layer1",
   "Layer2",
   "Layer3",
@@ -41,28 +46,28 @@ char layerNames[MAX_LAYER][10] = {
 };
 
 // Uses as second index the key index which is the led index
-char layerButtonFunc[MAX_LAYER][12][MAX_KEY_LENGTH] = {
+const char layerButtonFunc[MAX_LAYER][12][MAX_KEY_LENGTH] PROGMEM = {
   // Last defines max amount of chars for a name of a key
   { "L0L1", "L0M1", "L0R1",  // name: Layer 0 Left Button Row 1; Layer 0 Middle Button Row 1; Layer 0 Middle Button Row 1
-    "L0R2", "L0M2", "L0L2",
+    "L0L2", "L0M2", "L0R2",
     "L0L3", "L0M3", "L0R3",
-    "L0R4", "L0M4", "L0L4" },
+    "L0L4", "L0M4", "L0R4" },
   { "L1L1", "L1M1", "L1R1",
-    "L1R2", "L1M2", "L1L2",
+    "L1L2", "L1M2", "L1R2",
     "L1L3", "L1M3", "L1R3",
-    "L1R4", "L1M4", "L1L4" },
+    "L1L4", "L1M4", "L1R4" },
   { "L2L1", "L2M1", "L2R1",
-    "L2R2", "L2M2", "L2L2",
+    "L2L2", "L2M2", "L2R2",
     "L2L3", "L2M3", "L2R3",
-    "L2R4", "L2M4", "L2L4" },
+    "L2L4", "L2M4", "L2R4" },
   { "L3L1", "L3M1", "L3R1",
-    "L3R2", "L3M2", "L3L2",
+    "L3L2", "L3M2", "L3R2",
     "L3L3", "L3M3", "L3R3",
-    "L3R4", "L3M4", "L3L4" },
+    "L3L4", "L3M4", "L3R4" },
   { "L4L1", "L4M1", "L4R1",
-    "L4R2", "L4M2", "L4L2",
+    "L4L2", "L4M2", "L4R2",
     "L4L3", "L4M3", "L4R3",
-    "L4R4", "L4M4", "L4L4" },
+    "L4L4", "L4M4", "L4R4" },
 };
 
 typedef enum xPos {
@@ -110,7 +115,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(freeMemory());
+  //Serial.println(freeMemory()); //1669 byte free regardless of the number of layers
   readMatrix();
   delay(100);  // TODO real deboune
 
@@ -172,7 +177,7 @@ void keyPressed(byte rowIdx, byte colIdx) {
   }
 }
 
-void drawStringAtPosition(const char *buf, xPosition xPosition, yPosition yPosition) {
+void drawStringAtPosition(const char* buf, xPosition xPosition, yPosition yPosition) {
   int h = display.getFontAscent() - display.getFontDescent();
   int w = display.getStrWidth(buf);
 
@@ -188,26 +193,52 @@ void drawStringAtPosition(const char *buf, xPosition xPosition, yPosition yPosit
   display.print(buf);
 }
 
+void progMemStrCpy(const char str[], char buf[]) {
+  char c;
+  byte i = 0;
+  while ((c = pgm_read_byte(str++))) {
+    buf[i] = c;
+    i++;
+  }
+  buf[i] = '\0';
+}
+
 void setDisplayText() {
-  drawStringAtPosition(layerNames[currentLayer], CENTER, ROW0);
+  char layerBuf[LAYER_NAME_LENGTH + 1];
+  progMemStrCpy(layerNames[currentLayer], layerBuf);
+  drawStringAtPosition(layerBuf, CENTER, ROW0);
 
   display.drawLine(LEFT, HLINE1, RIGHT, HLINE1);
   display.drawLine(VLINE1, TOP, VLINE1, BOTTOM);
   display.drawLine(VLINE2, TOP, VLINE2, BOTTOM);
 
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(1, 2) - 3], LEFT, ROW1);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(1, 1) - 3], CENTER, ROW1);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(1, 0) - 3], RIGHT, ROW1);
+  char actionBuf[MAX_KEY_LENGTH + 1];
 
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(2, 2) - 3], LEFT, ROW2);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(2, 1) - 3], CENTER, ROW2);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(2, 0) - 3], RIGHT, ROW2);
+  progMemStrCpy(layerButtonFunc[currentLayer][0], actionBuf);
+  drawStringAtPosition(actionBuf, LEFT, ROW1);
+  progMemStrCpy(layerButtonFunc[currentLayer][1], actionBuf);
+  drawStringAtPosition(actionBuf, CENTER, ROW1);
+  progMemStrCpy(layerButtonFunc[currentLayer][2], actionBuf);
+  drawStringAtPosition(actionBuf, RIGHT, ROW1);
 
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(3, 2) - 3], LEFT, ROW3);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(3, 1) - 3], CENTER, ROW3);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(3, 0) - 3], RIGHT, ROW3);
+  progMemStrCpy(layerButtonFunc[currentLayer][3], actionBuf);
+  drawStringAtPosition(actionBuf, LEFT, ROW2);
+  progMemStrCpy(layerButtonFunc[currentLayer][4], actionBuf);
+  drawStringAtPosition(actionBuf, CENTER, ROW2);
+  progMemStrCpy(layerButtonFunc[currentLayer][5], actionBuf);
+  drawStringAtPosition(actionBuf, RIGHT, ROW2);
 
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(4, 2) - 3], LEFT, ROW4);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(4, 1) - 3], CENTER, ROW4);
-  drawStringAtPosition(layerButtonFunc[currentLayer][getLedIndex(4, 0) - 3], RIGHT, ROW4);
+  progMemStrCpy(layerButtonFunc[currentLayer][6], actionBuf);
+  drawStringAtPosition(actionBuf, LEFT, ROW3);
+  progMemStrCpy(layerButtonFunc[currentLayer][7], actionBuf);
+  drawStringAtPosition(actionBuf, CENTER, ROW3);
+  progMemStrCpy(layerButtonFunc[currentLayer][8], actionBuf);
+  drawStringAtPosition(actionBuf, RIGHT, ROW3);
+
+  progMemStrCpy(layerButtonFunc[currentLayer][9], actionBuf);
+  drawStringAtPosition(actionBuf, LEFT, ROW4);
+  progMemStrCpy(layerButtonFunc[currentLayer][10], actionBuf);
+  drawStringAtPosition(actionBuf, CENTER, ROW4);
+  progMemStrCpy(layerButtonFunc[currentLayer][11], actionBuf);
+  drawStringAtPosition(actionBuf, RIGHT, ROW4);
 }
